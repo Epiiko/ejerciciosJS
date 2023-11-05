61f323b2738029778304d9e9fd2f0ed6b2dad10a
Epikoo <ignacioandresdiazromero@gmail.com>
2023-11-05 23:20:28 +0100

    Ejercicios resueltos + index actualizado

--- PrimerTrimestre/Relacion1/ej16/ej16.js
+++ PrimerTrimestre/Relacion1/ej16/ej16.js
@@ -4,3 +4,40 @@
 // máximo.
 // b) Mejora el ejercicio haciendo que cuando pulsemos uno de los botones, este quede
 // bloqueado hasta que la barra del elemento progress/meter se complete del todo.
+document.querySelector("input").addEventListener("click", function () {
+    timerdescarga=setInterval("descargar()", 100)
+    this.disabled = true;
+})
+document.querySelectorAll("input")[1].addEventListener("click", function () {
+    timercarga=setInterval("cargar()", 100)
+    this.disabled = true;
+})
+
+function descargar() {
+    let barra = document.querySelector("progress");
+    if (barra.value + parseInt((Math.random() * 5) + 1) >= 100) {
+        barra.value = barra.max;
+        setTimeout(() => {
+            barra.value=0;
+            document.querySelector("input").disabled=false;
+            clearInterval(timerdescarga);
+            alert("La descarga ha finalizado")
+        }, 100);
+    } else {
+        barra.value += parseInt((Math.random() * 2) + 1);
+    }
+}
+function cargar(){
+    let barra = document.querySelector("meter");
+    if (barra.value + parseInt((Math.random() * 5) + 1) >= 100) {
+        barra.value = barra.max;
+        setTimeout(() => {
+            barra.value=0;
+            document.querySelectorAll("input")[1].disabled=false;
+            clearInterval(timercarga);
+            alert("La carga ha finalizado")
+        }, 500);
+    } else {
+        barra.value += parseInt((Math.random() * 2) + 1);
+    }
+}
