46359bbcbb4347cc4a57da5a9ada0ab5acecf9b6
Epikoo <ignacioandresdiazromero@gmail.com>
2024-02-05 00:34:08 +0100

    GOT casi listo

--- SegundoTrimestre/APIs/PruebaGOT/spa.js
+++ SegundoTrimestre/APIs/PruebaGOT/spa.js
@@ -1,46 +1,52 @@
-window.onload = () => {
-    function loadHouses(){
-      fetch("https://api.gameofthronesquotes.xyz/v1/houses").then(res=>res.json()).then(response=>console.log(response));
-      // return response;
-    }
-    function housesCards(){
-      const houses=loadHouses();
-      
-    }
-    loadHouses();
-    function loadPersons(){
-      const origen = "libros.json";
-      let salida = document.querySelector(".content");
-      // creo el objeto
-      xhr = new XMLHttpRequest();
-      //hago la peticion
-      xhr.open("GET", origen);
-      //   paso3
-      xhr.addEventListener("load", function (respuesta) {
-         respuesta =JSON.parse(respuesta.target.response);
-        let library= respuesta.library;
-        let libroHTML = "";
-       for (const  book of  library) {
-        console.log(book);
-        libroHTML += `<div class="card" style="width: 18rem;">
-        <img src="${book.cover}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.synopsis}</p>
-          <a href="#" class="btn btn-primary">Ver libro</a>
-        </div>
-      </div>
-      <br>`;
-       }
-       salida.innerHTML=libroHTML;
-      });
-      xhr.addEventListener("error", function (respuesta) {
-        console.log("ERROR");
-      });
-      xhr.send();
-      //   esto no es asincrona
-      console.log("Soy la ultima en aparecer...");
-    }
-    
-  };
-  
+
+function loadHouses() {
+  let resu = fetch("https://api.gameofthronesquotes.xyz/v1/houses").then(res => res.json()).then(response => addHouses(response)).catch(err => console.log("DSHAD"));
+  console.log(resu);
+}
+function addHouses(response) {
+  let hause_div = document.querySelector("#houses");
+  response.forEach(element => {
+    hause_div.innerHTML += `<div class="card" id="${element.slug}">
+        <img src="media/${element.slug}.png" alt="" id="img_${element.slug}" class="houses_logos">
+        <h3 class="text">${element.slug}</h3>
+    </div>`
+  });
+}
+function addCharacters(response) {
+  let person_div = document.querySelector("#persons");
+  response.forEach(element => {
+    person_div.innerHTML += `<div class="card" id="${element.slug}">
+        <img src="media/characters/${element.slug}.jpg" alt="" id="img_${element.slug}" class="houses_logos">
+        <h3 class="text">${element.slug}</h3>
+    </div>`
+  });
+}
+
+function loadCharacters() {
+  let resu = fetch("https://api.gameofthronesquotes.xyz/v1/characters").then(res => res.json()).then(response => addCharacters(response)).catch(err => console.log("DSHAD"));
+  console.log(resu);
+}
+
+function loadQuotes() {
+  let resu = fetch("https://api.gameofthronesquotes.xyz/v1/random/5")
+    .then(res => res.json())
+    .then(response => addQuotes(response))
+    .catch(err => console.log("DSHAD"));
+  console.log(resu);
+}
+
+function addQuotes(response) {
+  let quotes_div = document.querySelector("#quotes");
+  response.forEach(element => {
+    quotes_div.innerHTML += `<div class="card">
+        <h3 class="text"> " ${element.sentence} "</h3>
+        <p>- ${element.character.name}</p>
+        <p> ${element.character.house.name ?? ''}</p>
+    </div>`;
+  });
+}
+
+loadHouses();
+loadCharacters();
+loadQuotes();
+
